dba14b2f9b08ada5ae92692e166c9129e5e87892
Amber-Pittman <amber@amberpittman.com>
2020-04-28 19:54:44 -0400

    Get and Post working with database

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -14,44 +14,29 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res, next) => {
     try {
-        const {id} = req.params.id
-        const car = await db("cars").where({ id }).first()
+    const carById = await db("cars").where("id", req.params.id)
 
-        res.json(car)
-    } catch (error) {
-        next(error)
-    }
-})
-
-router.put("/:id", async (req, res, next) => {
-    try {
-        const payload = {
-            vin: req.body.vin,
-            make: req.body.make,
-            model: req.body.model,
-            mileage: req.body.mileage,
-            transmission: req.body.transmission,
-            title: req.body.title
-        }
-
-        const {id} = req.params.id
-
-        await db("cars").where({id}).update(payload)
-
-        const updatedCar = await db("cars").where({id}).first()
-
-        res.json(updatedCar)
-
-    } catch (err) {
+    res.json(carById)
+    } catch(err) {
         next(err)
     }
 })
 
 router.post("/", async (req, res, next) => {
+    const {VIN, Make, Model, Mileage, Transmission, Title} = req.body
+
     try {
-        const carInfo = req.body
-        const id = await db("cars").insert(carInfo)
-        const newCar = await db("cars").where({id})
+        const payload = {
+            VIN,
+            Make,
+            Model,
+            Mileage,
+            Transmission,
+            Title
+        }
+       
+        const [id] = await db("cars").insert(payload)
+        const newCar = await db("cars").where("id", id).first()
 
         res.status(201).json(newCar)
 
@@ -60,10 +45,40 @@ router.post("/", async (req, res, next) => {
     }
 })
 
+
+router.put("/:id", async (req, res, next) => {
+    const {VIN, Make, Model, Mileage, Transmission, Title} = req.body
+
+    try {
+        const payload = {
+            VIN,
+            Make,
+            Model,
+            Mileage,
+            Transmission,
+            Title
+        }
+         
+        const {id} = req.params.id
+
+        await db("cars").where({id}).update(payload)
+
+        const updatedCar = await db("cars").where({id}).first()
+
+        res.json(updatedCar)
+
+    } catch(err) {
+        next(err)
+    }
+})
+
+
 router.delete("/:id", async (req, res, next) => {
     try {
         const {id} = req.params.id
-        await db("cars").where({id}).delete()
+        await db("cars").where({id}).del()
+
+        res.status(204).end()
         // Wanted to use truncate but wasn't sure if I should reset 
         // the IDs here decided against it. Better safe than sorry.
     } catch(err) {
